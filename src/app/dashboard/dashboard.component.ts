94b7ff3f9641d4eff652114be0cfb1bbfc03310f
dsjankelow <dsjankelow@gmail.com>
2019-07-22 10:28:28 +0200

    jquery working to push selected worker into assisgned mission

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,7 @@ import * as _ from 'underscore';
 
 declare let jQuery: any;
 declare let M: any;
+var selectedClient: any = [];
 
 @Component({
   selector: 'app-dashboard',
@@ -18,19 +19,21 @@ export class DashboardComponent implements OnInit {
 
   private clients: Clients[] = [];
   private scoutMissions: ScoutMissions[] = [];
+  staticScoutMissions: any = [
+    { "count": 7, "next": null, "previous": null, "results": [{ "id": 1376, "farm_id": 4006, "title": "Scout", "date": "2018-09-11", "complete": false }, { "id": 1375, "farm_id": 4006, "title": "Scout 8", "date": "2018-09-30", "complete": false }, { "id": 1373, "farm_id": 4006, "title": "Scout 5", "date": "2018-10-03", "complete": false }, { "id": 1372, "farm_id": 4006, "title": "Scout 4", "date": "2018-09-23", "complete": false }, { "id": 1371, "farm_id": 4006, "title": "Scout 3", "date": "2018-09-15", "complete": false }, { "id": 1370, "farm_id": 4006, "title": "Scout 2", "date": "2018-09-11", "complete": false }, { "id": 1369, "farm_id": 4006, "title": "Scout 1", "date": "2018-09-14", "complete": false }] }
+  ];
 
-  selectedClient: any = [];
+  staticClients: any = [{"count":3,"next":null,"previous":null,"results":[{"id":6253,"name":"Farmer Developer","email":"developertest@test.com","user_id":6378},{"id":6254,"name":"Farm Worker 1","email":"developer2test@test.com","user_id":6379},{"id":6255,"name":"Farm Worker 2","email":"developer3test@test.com","user_id":6380}]}]
+
   constructor(private coreService: CoreService,
     private activatedRoute: ActivatedRoute, private _http: HttpClient,
-    ) {
+  ) {
 
     this.setClients();
     this.setScoutMissions();
 
-    console.log(this.selectedClient)
 
   }
-
 
   setClients() {
     this.clients = [];
@@ -41,12 +44,46 @@ export class DashboardComponent implements OnInit {
 
         setTimeout(function () {
           jQuery('select').material_select();
+
+
+
+
+          let context;
+          // this.selectedClient = 26255;
+
+
+          jQuery('.testt').bind("change", (e) => {
+            let dataIndex = parseInt(e.target.attributes['data-index'].value);
+            let selectedObj: any = { 'index': dataIndex, 'id': e.target.value };
+
+            if (selectedClient[dataIndex]) {
+              selectedClient[dataIndex] = selectedObj;
+            } else {
+              selectedClient.push(selectedObj);
+            }
+          })
+
         }, 200);
       },
       err => {
+
         console.log('Error fetching clients' + this.clients);
       }
     )
+  }
+
+
+
+  assignWorkers() {
+    debugger;
+    _.each(this.scoutMissions, function (item, key) {
+      debugger
+      this.scoutMissions[key]['workerObj'] = selectedClient[key];
+      console.log(this.scoutMissions);
+
+
+      debugger;
+    },this);
   }
 
   setScoutMissions() {
@@ -62,22 +99,15 @@ export class DashboardComponent implements OnInit {
       })
   }
 
-  onClientChange(val) {
-    debugger
-    this.selectedClient = val
-    console.log(val);
 
-  }
-  selectedScoutMissions(client) {
-    debugger;
-    // this.selectedClient.push(i);
-  }
+
+
 
   ngOnInit() {
     jQuery(document).ready(function () {
       jQuery('.tabs').tabs();
     });
 
-    // this.selectedClient = 26255;
+
   }
 }
